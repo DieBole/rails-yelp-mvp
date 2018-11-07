2cdd948e757ee4960c39ef720f1747ca68e355e0
Diego Bolettieri <diego@bebetterhotels.com>
2018-11-07 16:59:29 +0100

    finished controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+  },
+  {
+    name:         'DaRose',
+    address:      'Cour Leclerc, Langon',
+    category:     'french',
+    phone_number: '06 45 87 90 22'
+  },
+  {
+    name:         'Leon de Bruxelles',
+    address:      'Rue Martinique 44, Bordeaux',
+    category:     'belgian',
+    phone_number: '06 45 62 00 34'
+  },
+  {
+    name:         'Bonsai',
+    address:      'Cour Sttutenberg, Bordeaux',
+    category:     'japanese',
+    phone_number: '06 77 10 69 03'
+  }
+]
+Restaurant.create!(restaurants_attributes)
